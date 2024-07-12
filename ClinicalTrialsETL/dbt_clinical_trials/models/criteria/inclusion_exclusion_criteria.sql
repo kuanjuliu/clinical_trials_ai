acd44280e4cc9b9648af4b9642223ef98c614db2
Kuan Ju Liu <kujulu@gmail.com>
2024-07-12 04:46:04 -0400

    Update inclusion_exclusion_criteria.sql

--- ClinicalTrialsETL/dbt_clinical_trials/models/criteria/inclusion_exclusion_criteria.sql
+++ ClinicalTrialsETL/dbt_clinical_trials/models/criteria/inclusion_exclusion_criteria.sql
@@ -12,7 +12,9 @@
 with cte as (
     SELECT
         protocol_section__identification_module__nct_id AS nct_id
---        , protocol_section__eligibility_module__eligibility_criteria
+        , protocol_section__eligibility_module__minimum_age AS minimum_age
+        , protocol_section__eligibility_module__maximum_age AS maximum_age
+        , protocol_section__eligibility_module__sex AS gender
         , regexp_extract(
             protocol_section__eligibility_module__eligibility_criteria
             , '(?i)inclusion( criteria)?:?(?-i)[\n\r]*([\S\s]*?)(?i)exclusion( criteria)?(?-i)'
@@ -25,6 +27,7 @@ with cte as (
           ) AS exclusion_criteria
         , concat('https://clinicaltrials.gov/study/', nct_id) as URL
     FROM {{ source('clinical_trials', 'clinical_trials') }}
+--    WHERE TRY_CAST(protocol_section__status_module__last_update_post_date_struct__date AS DATE) >= date_add (today(), - INTERVAL 1 MONTH)
 )
 select *
 from cte
